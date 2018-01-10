3bf929cd49dabb7db18005e04a23fdb59dd3ca8b
Vladislavs Dovgalecs <vladislavsd@voicebox.com>
2018-01-09 17:53:35 -0800

    Initial commit - Synthetic data generation

--- InferNET-ModelSelection/Model.cs
+++ InferNET-ModelSelection/Model.cs
@@ -3,10 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrosoftResearch.Infer;
+using MicrosoftResearch.Infer.Distributions;
 
 namespace InferNET_ModelSelection
 {
     class Model
     {
+        public Model()
+        {
+
+        }
+
+        // generate synthetic data
+        public static void GenerateData(int n, double modelProb, double probTreated, double probControl, double probRecovery,
+                                        out bool[] treatedData, out bool[] controlData)
+        {
+            treatedData = new bool[n];
+            controlData = new bool[n];
+
+            Bernoulli model = new Bernoulli(modelProb);
+
+            Bernoulli treated = new Bernoulli(probTreated);
+            Bernoulli control = new Bernoulli(probControl);
+
+            Bernoulli recovery = new Bernoulli(probRecovery);
+
+            for (int i = 0; i < n; i++)
+            {
+                if (model.Sample())
+                {
+                    // has effect
+                    treatedData[i] = treated.Sample();
+                    controlData[i] = control.Sample();
+                }
+                else
+                {
+                    // no effect
+                    treatedData[i] = recovery.Sample();
+                    controlData[i] = recovery.Sample();
+                }
+            }
+        }
     }
 }
--- InferNET-ModelSelection/Program.cs
+++ InferNET-ModelSelection/Program.cs
@@ -10,6 +10,17 @@ namespace InferNET_ModelSelection
     {
         static void Main(string[] args)
         {
+            //
+            // Parameters for synthetic data generation
+            //
+
+            int n = 100;
+            double probModel = 0.75;
+            double probTreated = 0.90;
+            double probControl = 0.55;
+            double probRecovery = 0.50;
+
+            Model.GenerateData(n, probModel, probTreated, probControl, probRecovery, out bool[] treatedData, out bool[] controlData);
         }
     }
 }
